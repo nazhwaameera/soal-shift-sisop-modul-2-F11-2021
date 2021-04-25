379cd5b1548910ea7d936001e3315679914fa3cf
juliettaanastasia <74484044+juliettaanastasia@users.noreply.github.com>
2021-04-25 11:59:47 +0700

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -11,8 +11,30 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-void killer(char arr[]);
-int main(int argc, char *argv[])
+void killer(char *argv[], int pid)
+{
+    // 3d untuk membuat file killer.sh
+    FILE *kill = fopen("killer.sh", "w");
+    fprintf(kill, "#/bin/bash\n");
+
+    // 3e
+    // arg -z untuk menghentikan semua operasi
+    if(strcmp(argv[1], "-z") == 0)
+    {
+        fprintf(kill, "killall -9 soal3\n");
+    }
+    // arg -x untuk kill proses utama --> pid
+    else if(strcmp(argv[1], "-x") == 0)
+    {
+        fprintf(kill, "kill -9 %d\n", pid);
+    }
+
+    // delete folder setelah selesai di zip
+    fprintf(kill, "rm killer.sh\n");
+    fclose(kill);
+}
+
+int main(int argc, char** argv)
 {
     pid_t pid, sid;
     pid = fork();
@@ -27,13 +49,15 @@ int main(int argc, char *argv[])
     }
     
     umask(0);
-    killer(argv[1]);
     sid = setsid();
     
     if(sid < 0)
     {
         exit(EXIT_FAILURE);
     }
+    
+    // untuk menjalankan argumen -z dan -x
+    killer(argv, (int)getpid());
     
     close(STDIN_FILENO);
     close(STDOUT_FILENO);
@@ -86,28 +110,56 @@ int main(int argc, char *argv[])
                     if(child_id_3 == 0)
                     {
                         chdir(tanggal);
-                        execl("/usr/bin/wget", "wget", "-O", tanggal2, alamat, NULL);
+                        char *f[] = {"wget", "-bq", alamat, "-O" , tanggal2, NULL};
+                        execv("/usr/bin/wget", f);
                     }
                     sleep(5); // gambar di download tiap 5 detik
                 }
-                char zip[50]; // untuk mengubah folder menjadi bentuk zip
+                
+                // isi file status.txt
+                char kata[50] = "Download Success";
+                char word;
+                      
+                // caesar cipher
+                for(int i = 0; i < strlen(kata); i++)
+                {
+                    word = kata[i];
+                    if(word == ' ')
+                    {
+                        continue;
+                    }
+                    if(word >= 'a' && word <= 'z')
+                    {
+                        word += 5;
+                        if(word > 'z')
+                        {
+                            word = word - 'z' + 'a' - 1;
+                        }
+                        kata[i] = word;
+                    }
+                    else if(word >= 'A' && word <= 'Z')
+                    {
+                        word += 5;
+                        if(word > 'Z')
+                        {
+                            word = word - 'Z' + 'A' - 1;
+                        }
+                        kata[i] = word;
+                    }
+                }
+                char file[100];
+                sprintf(file, "%s/status.txt", tanggal);
+                FILE *status = fopen(file, "w");
+                fputs(kata, status);
+                fclose(status);
+                
+                // untuk mengubah folder menjadi bentuk zip
+                char zip[50];
                 sprintf(zip, "%s.zip", tanggal);
                 char *argv2[5] = {"zip", "-rm", zip, tanggal, NULL};
                 execv("/usr/bin/zip", argv2);
             }
         }
-        // program berjalan setiap 40 detik
         sleep(40);
     }
-}
-
-void killer(char arr[])
-{
-    FILE *kill;
-    kill = fopen("killer.sh", "w");
-    if(strcmp(arr, "-z") == 0)
-        fprintf(kill, "rm $0\n#!/bin/bash\nkill -9 -%d", getpid());
-    else if(strcmp(arr, "-x") == 0)
-        fprintf(kill, "rm $0\n#!/bin/bash\nkill %d", getpid());
-    fclose(kill);
 }
